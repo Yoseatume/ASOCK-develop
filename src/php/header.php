2fae142ef4b22d41850809b76ce144551f0b9b97
Aoi1591 <2401906@s.asojuku.ac.jp>
2025-06-13 13:37:31 +0900

    ヘッダー
    ヘッダー作成

---
+++ src/php/header.php
@@ -0,0 +1,14 @@
+<?php
+ echo '<!DOCTYPE html>';
+ echo '<html lang="ja">';
+ echo '<head>';
+ echo '<meta charset="utf-8">';
+ echo '<title>ヘッダー</title>';
+ echo '<link rel="stylesheet" href="../CSS/header.css">';
+ echo '</head>';
+ echo '<body>';
+ echo '<header>';
+ echo '<a href="#" name="home" id="home_icon"><img src=../img/home.png alt="家のアイコン"></a>';
+ echo '<a href="#" name="admin" id="admin_icon"><img src=../img/admin.png alt="管理者のアイコン"></a>';
+ echo '</header>';
+?>
